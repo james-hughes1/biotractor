6ea7c7deb5fe151143ecc84e8b4defc7597a16d0
James Hughes <jh2284@cam.ac.uk>
2024-09-22 14:18:37 +0100

    Feat: Add a pause button.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 // Function to change the color of the square
 const changeColor = () => {
-    document.getElementById("hiddenMessage").style.display = "block";
-
     const square = document.getElementById("colorSquare");
-    
-    // Array of colors to cycle through
+
     const colors = ["red", "blue", "yellow"];
     let currentColorIndex = 0;
 
-    // Set an interval to change the color every 10 seconds
-    setInterval(() => {
-        // Move to the next color in the array
+    intervalId = setInterval(() => {
         currentColorIndex = (currentColorIndex + 1) % colors.length;
-        
-        // Change the background color of the square
         square.style.backgroundColor = colors[currentColorIndex];
-    }, 10000); // 10000ms = 10 seconds
+    }, 1000);
 }
 
-// Adding event listener to the button
 document.getElementById("startButton").addEventListener("click", () => {
     changeColor();
-    document.getElementById("startButton").disabled = true; // Disable button after clicking
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("pauseButton").disabled = false;
+});
+
+document.getElementById("pauseButton").addEventListener("click", () => {
+    clearInterval(intervalId);
+    document.getElementById("pauseButton").disabled = true;
+    document.getElementById("startButton").disabled = false;
 });
